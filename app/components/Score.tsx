1d1201c610075a14a59a3e55e901b0e49f48e5eb
Ed Wong <edmondwong6279@gmail.com>
2024-10-09 23:15:58 +0100

    feat(#25): 💄 adding box shadow to score component

--- app/components/Score.tsx
+++ app/components/Score.tsx
@@ -3,7 +3,7 @@
  */
 export default function Score({ score }: { score: number }) {
   return (
-    <section className="flex flex-col items-center justify-around bg-white px-5 py-2 md:px-12 md:py-4 rounded-md">
+    <section className="flex flex-col items-center justify-around bg-white px-5 py-2 md:px-12 md:py-4 rounded-md shadow-md">
       <p className="text-grey font-semibold text-xs md:text-base tracking-wider">
         SCORE
       </p>
